7b9aa0e5efca0113eb722b04a26fed70a3673555
AlexisG <alexis.gruny60@gmail.com>
2018-06-21 08:40:20 +0200

    ajout de commentaire

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 function testAge(){
   var age = document.getElementById("age").value;
+  // test regex
   if (/^[0-9]+$/.test(age) == true){
+    // condition si mineur
     if (age < 18){
       alert('Vous etes mineur!');
     }
+    // condition si majeur
     else if(age < 120){
       alert('Vous etes majeur!');
     }
+    // si autre
     else{
       alert('Vous etes mort!')
+    }
+    // regex false
+    else{
+      alert('veuillez entrer un nombre')
     }
   }
 }
